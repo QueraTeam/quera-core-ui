d79cd5792b5eec8fb2bdb75888e66a03568ee19f
Mohammadreza Varasteh <varastehmr@gmail.com>
2023-08-20 11:26:43 +0330

    bump version and add branding

---
+++ .storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/manager-api";
+import Theme from "./theme";
+
+addons.setConfig({
+  theme: Theme,
+});
---
+++ .storybook/theme.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+  brandTitle: "Quera",
+  brandUrl: "https://quera.org",
+  brandImage: "/images/branding.png",
+  brandTarget: "_blank",
+});
